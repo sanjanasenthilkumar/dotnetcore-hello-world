85747c6239a6089b038ab9d4ea9dac7af41efe03
sanjanasenthilkumar <81677782+sanjanasenthilkumar@users.noreply.github.com>
2021-04-01 14:45:42 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,14 @@
-﻿using System;
-using Newtonsoft.Json;
-
-namespace yello
+// Hello World! program
+using System;
+ 
+namespace HelloWorld
 {
-
-    class Animal {
-        public string type;
-        public string name;
-
-        public void setType(string newType){
-            type = newType;
-        }
-
-        public void setName(string newName){
-            name = newName;
-        }
-
-    }
-
-    class Program
-    {
+    class Hello {         
         static void Main(string[] args)
         {
-            var animal = new Animal();
-            animal.setType("Pedobear");
-            animal.setName("Jorma");
-
-            var animalJson = JsonConvert.SerializeObject(animal);
-
-            Console.WriteLine(animalJson);
+            Console.WriteLine("Hello World!");
         }
     }
 }
+
+       
